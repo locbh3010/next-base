d69e5be33db79652b9aa6c5b9a9de23ee0df8ab9
locbh3010 <locbh3010@gmail.com>
2023-06-06 14:45:10 +0700

    add analyze for build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.NODE_ENV === 'production',
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withBundleAnalyzer({
+  reactStrictMode: true,
+});
+
+module.exports = nextConfig;
