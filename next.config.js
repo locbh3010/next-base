6f0a0183eae991f33f42c920c2eb1f3b7f7e7c04
locbh3010 <locbh3010@gmail.com>
2023-06-06 15:12:04 +0700

    add windi css

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: true,
+  enabled: process.env.NODE_ENV === 'production',
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
-});
+  productionBrowserSourceMaps: false,
+  poweredByHeader: false,
 
-module.exports = nextConfig;
+  webpack(config) {
+    config.plugins.push(new WindiCSSWebpackPlugin());
+    return config;
+  },
+
+  transpilePackages: ['lodash'],
+});
