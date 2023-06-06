f1d6eee9d8d90be9e102e0cc01cb2e4361d9f900
locbh3010 <locbh3010@gmail.com>
2023-06-06 15:03:21 +0700

    add tailwind css

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: true,
+  enabled: process.env.NODE_ENV === 'production',
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
+  productionBrowserSourceMaps: false,
+  poweredByHeader: false,
+
+  transpilePackages: ['lodash'],
 });
 
 module.exports = nextConfig;
